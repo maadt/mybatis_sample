83e3febedc06802e8192647b73e263075e777f3c
maadt <147260993+maadt@users.noreply.github.com>
2024-01-30 17:56:43 +0900

    Lesson03実装完了

--- src/main/java/com/example/entity/Course.java
+++ src/main/java/com/example/entity/Course.java
@@ -1,11 +1,14 @@
 package com.example.entity;
 
+import java.util.List;
+
 //Spring Data JPAとの差分として、アノテーションで定義していたテーブルのカラムとEntityのフィールドの紐づけがない
 
 public class Course {
     private int id;
     private String name;
     private CourseDetail courseDetail;
+    private List<Chapter> chapters; // １対多なのでList型
 
     public int getId() {
         return id;
@@ -24,5 +27,11 @@ public class Course {
     }
     public void setCourseDetail(CourseDetail courseDetail) {
         this.courseDetail = courseDetail;
+    }
+    public List<Chapter> getChapters() {
+        return chapters;
+    }
+    public void setChapters(List<Chapter> chapters) {
+        this.chapters = chapters;
     }
 }
